c90c193ae5f93d9ed2946912de03c84efa1c99e1
christine parsons <clong9712@gmail.com>
2024-03-28 22:33:28 -0400

    BACKEND: Set up blocks module

--- backend/src/blocks/blocks.module.ts
+++ backend/src/blocks/blocks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BlocksController } from './controllers/blocks/blocks.controller';
 import { BlocksService } from './services/blocks/blocks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Block } from 'src/entities/block.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Block])],
   controllers: [BlocksController],
   providers: [BlocksService],
+  exports: [BlocksService],
 })
 export class BlocksModule {}
