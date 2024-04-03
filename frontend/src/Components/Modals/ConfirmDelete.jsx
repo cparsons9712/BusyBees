443aba8939539c8b2861b353a0fc22f06fdaed5e
christine parsons <clong9712@gmail.com>
2024-04-02 22:20:58 -0400

    STYLE: confirm delete modal and day of week navigation on block page

--- frontend/src/Components/Modals/ConfirmDelete.jsx
+++ frontend/src/Components/Modals/ConfirmDelete.jsx
@@ -1,18 +1,19 @@
 import { useDeleteBlock } from "../../Hooks/useBlockQueries"
 import { useModal } from "../../Context/Modal"
+import '../../Styling/confirmDelete.css'
 
 const ConfirmDelete = ({resource}) => {
     const { mutate, isError, error} = useDeleteBlock()
     const {hideModal} = useModal()
 
     return (
-        <div>
-           <h2>Caution</h2>
-            <p>Deleting this {resource.type} is permanent and can not be reversed. </p>
-            <div>Are you sure you want to delete the {resource.type} "{resource.title}"?  </div>
+        <div className="confirmDelete">
+           <h2 className="cursive caution">Caution!!!</h2>
+            <p className="handwriting deleteText">Deleting this {resource.type} is permanent and can not be reversed. </p>
+            <div className="handwriting deleteText">Are you sure you want to delete the {resource.type} "{resource.title}"?  </div>
             <div className="deleteBtnBar">
-                <button onClick={hideModal}>Keep it</button>
-                <button onClick={()=>{mutate({id: +resource.id})}}>Delete it</button>
+                <button onClick={hideModal} className="handwriting deleteText deleteBtn">Keep it</button>
+                <button onClick={()=>{mutate({id: +resource.id})}} className="handwriting deleteText deleteBtn">Delete it</button>
             </div>
             {isError && <div> {error.message} </div>}
 
