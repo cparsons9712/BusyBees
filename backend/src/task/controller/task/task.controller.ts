ce25e68f53cb9bf83085d295d0d5d9b7f15b54e3
christine parsons <clong9712@gmail.com>
2024-04-03 00:02:56 -0400

    BACKEND: set up task module

--- backend/src/task/controller/task/task.controller.ts
+++ backend/src/task/controller/task/task.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthenticatedGuard } from 'src/auth/utils/LocalGuard';
 import { TaskService } from 'src/task/service/task/task.service';
 import { GetUser } from 'src/utils/get-user.decorator';
+import { Task } from 'src/entities/task.entity';
 
 @Controller('task')
 export class TaskController {
@@ -13,3 +14,14 @@ export class TaskController {
     return await this.taskService.getTaskByUserId(user.id);
   }
 }
+
+
+// getTaskById
+
+// getUnassignedTask
+
+// MakeaTask
+
+// EditATask
+
+// DeleteATask
--- backend/src/task/task.module.ts
+++ backend/src/task/task.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './service/task/task.service';
 import { TaskController } from './controller/task/task.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from 'src/entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
+  controllers: [TaskController],
   providers: [TaskService],
-  controllers: [TaskController]
+  exports: [TaskService],
 })
 export class TaskModule {}
