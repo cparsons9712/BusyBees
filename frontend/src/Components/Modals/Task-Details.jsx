c5c7b2cfd2b955c1d9a468d5b146521ffb1fb28f
christine parsons <clong9712@gmail.com>
2024-04-07 16:34:46 -0400

    FRONTEND: Create toggle functionality for show subtask/ add subtask mini forms

--- frontend/src/Components/Modals/Task-Details.jsx
+++ frontend/src/Components/Modals/Task-Details.jsx
@@ -2,19 +2,22 @@ import { useModal } from "../../Context/Modal";
 import CreateEditTask from "./Create-Edit-Task";
 import ConfirmDelete from "./ConfirmDelete";
 import { useState, useEffect } from "react";
-import "../../Styling/subtask.css"
+import "../../Styling/subtask.css";
 
 const TaskDetails = ({ task, blockTitle }) => {
   const [statusColor, setStatusColor] = useState();
+  const [showSubtask, setShowSubtask] = useState(true)
+  const [showAddST, setShowAddST] = useState(false);
   const { showModal } = useModal();
+
   const timeUnitConversion = {
     1: "day",
     7: "week",
     30: "month",
     91: "quarter",
   };
-  useEffect(() => {
 
+  useEffect(() => {
     if (task.status) {
       setStatusColor("gold");
     } else {
@@ -22,71 +25,88 @@ const TaskDetails = ({ task, blockTitle }) => {
     }
   }, [task.status]);
 
+  const showAddSubtask = () => {
+    if(showAddST){
+      return(
+        <div className="addStForm">
+          <div className="addStTitleLabel">Title</div>
+          <textarea
+            placeholder="Subtask Title"
+            maxLength="60"
+            className="stTitleInput"
+          />
+          <div className="STButtonBar">
+            <div className="STButton" onClick={()=>setShowAddST(false)}>
+              Cancel
+            </div>
+            <div className="STButton">Save</div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  const showSubtaskList = () => {
+    if (showSubtask) {
+      return (
+        <div className="tdSubtaskList">
+        {getSubtask()}
+
+        {showAddST ?
+          showAddSubtask()
+            :
+          <div
+            className="addSTButton"
+            onClick={()=>setShowAddST(true)}
+          >
+            +
+          </div>
+        }
+      </div>
+      )
+    }
+  }
+
+
   const getSubtask = () => {
     return task?.subtasks?.length ? (
-      task.subtasks.map((st) => (
-        <div className="stTitle">
-          {st.title}
-        </div>
-      ))
+      task.subtasks.map((st) => <div className="stTitle">{st.title}</div>)
     ) : (
-      <div className="stTitle">No subtask to display ⋮</div>
+      <div className="stTitle">No subtask to display </div>
     );
-  }
+  };
 
   return (
     <div className="ceContainer">
       <div className="tdHeader">
-
         <div className={`tdStatusHexagon ${statusColor}`}>
-
-          <div className="tdstatusText">
-            {task.status ? "Done" : "To-Do"}
-          </div>
-
+          <div className="tdstatusText">{task.status ? "Done" : "To-Do"}</div>
         </div>
 
-        <div className="tdTitle cursive">
-          {task.title}
-        </div>
-
+        <div className="tdTitle cursive">{task.title}</div>
       </div>
 
       <div className="taskBody">
+        <div className="taskBlockLabel">Block</div>
 
-        <div className="taskBlockLabel">
-          Block
-        </div>
+        <div className="taskBlockTitle">{blockTitle ? blockTitle : "None"}</div>
 
-        <div className="taskBlockTitle">
-          {blockTitle ? blockTitle : "None"}
-        </div>
-
-        <div className="taskBlockLabel">
-          Repeat
-        </div>
+        <div className="taskBlockLabel">Repeat</div>
 
         <div className="taskBlockTitle">
-          {task.repeatFrequency ?
-            `Every ${task.repeatFrequency} ${timeUnitConversion[task.timeUnit]}`
-              :
-            "Never"
-          }
+          {task.repeatFrequency
+            ? `Every ${task.repeatFrequency} ${
+                timeUnitConversion[task.timeUnit]
+              }`
+            : "Never"}
         </div>
 
         <div className="tdSubtaskcont">
-          <div className="tdToggleSubtask">
+          <div className="tdToggleSubtask" onClick={()=>setShowSubtask(!showSubtask)}>
             Subtask
           </div>
 
-          <div className="tdSubtaskList">
-
-              {getSubtask()}
-
-              <div className="addSTButton">
-                +
-              </div>
-          </div>
+          {showSubtaskList()}
 
         </div>
 
